542f7af1e2dc1c9d2a2c003bc505177ea877fa61
Brijesh SHiroya <brijesh.shiroya.reversebits@email.com>
2025-01-23 18:19:18 +0530

    Pattern-12 Done..

---
+++ pattern_12.js
@@ -0,0 +1,20 @@
+for(let i=5; i>=1; i--){
+    a = ''
+    for(let j=1; j<=(5-i); j++){
+        a = a + " "
+    }
+    for(let k=1; k<=i; k++){
+        a = a + "* "
+    }
+    console.log(a);
+}
+for(let i=1; i<=5; i++){
+    a = ''
+    for(let j=1; j<=(5-i); j++){
+        a = a + " "
+    }
+    for(let k=1; k<=i; k++){
+        a = a + "* "
+    }
+    console.log(a);
+}
